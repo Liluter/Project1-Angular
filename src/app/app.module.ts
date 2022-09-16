ef497c077991cae80c1956d9eab09513f7978354
liluter <karolawdziewicz@gmail.com>
2022-09-16 09:29:53 +0200

    50 point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShopingListComponent } from './features/shoping-list/shoping-list.component';
+import { RecipeBookComponent } from './features/recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShopingListEditComponent } from './shopping-list/shoping-list-edit/shoping-list-edit.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { RecipiesComponent } from './recipies/recipies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShopingListComponent,
+    RecipeBookComponent,
+    ShoppingListComponent,
+    ShopingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipiesComponent
   ],
   imports: [
     BrowserModule
